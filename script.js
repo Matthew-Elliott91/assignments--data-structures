226ea37fb405a8ca04dff23373648828b9e061a1
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-19 14:08:51 +0000

    completed asignments for nullish coalescing operator, logical assaignment operators, the for of loop and enhanced object literals

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const printBookAuthorsCount = function (title, ...authors) {
 
 printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
 
-//5.1
+// 5.1
 
 // Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
 
@@ -100,9 +100,89 @@ const hasExamplesInJava = function (book) {
   return book.programmingLanguage === "Java" || "no data availible";
 };
 
-//5.2 Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+// 5.2 Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
 
 for (let i = 0; i < books.length; i++) {
   books[i].onlineContent &&
     console.log(`"${books[i].title}" provides online content`);
 }
+
+// 6.1 There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent ??
+    console.log(`${books[i].title} provides no data about its online content`);
+}
+
+// 7.1 Some of the book objects from the books array are missing the edition property. Loop over the books array, and assign this property with a number 1 (if it doesn't already exist). Use logical assignment operators.
+
+// 7.2 Some of the book objects from the books array have the highlighted property, which by default is set to true. Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.
+
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ||= 1;
+}
+
+// Use the &&= operator (tip: you may also need the ! operator)
+
+for (let i = 0; i < books.length; i++) {
+  books[i].highlighted &&= !(books[i].thirdParty.goodreads < 4.2);
+}
+
+// 8.1 Use the for-of loop to loop over the books array and sum the pages of all books. Use the pageSum variable below, and the pages property of the book objects.
+
+let pageSum = 0;
+
+for (let book of books) {
+  pageSum += book.pages;
+}
+
+// 8.2 Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
+
+// Remember that each book object has the author property, which can be a string (if there is only a single author) or an array (if there are multiple authors). You may need to use the typeof operator. You can also use multiple loops if needed. The allAuthors array should have just one level (no nested arrays).
+
+const allAuthors = [];
+
+for (const book of books) {
+  if (typeof book.author === "string") {
+    allAuthors.push(book.author);
+  } else {
+    for (const author of book.author) {
+      allAuthors.push(author);
+    }
+  }
+}
+
+// 8.3
+
+for (const [i, e] of allAuthors.entries()) {
+  console.log(`${i + 1}. ${e}`);
+}
+
+// 9.1 Below is the bookData array that contains other arrays. Each inner array consists of the property name (first element), and the value (second element). For example, in ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the property name, and 'Computer Networking: A Top-Down Approach' is meant to be the value assigned to that property name.
+
+// Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already.
+
+const bookData = [
+  ["title", "Computer Networking: A Top-Down Approach"],
+  ["author", ["James F. Kurose", "Keith W. Ross"]],
+  ["publisher", "Addison Wesley"],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+// 9.2 Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way.
+
+const pages = 880;
+
+const newBook2 = {
+  title: "The C Programming Language",
+  author: ["Brian W. Kernighan", "Dennis M. Ritchie"],
+  pages,
+};
+
+console.log(newBook2.pages);
